be920d808b8879a9351e694d1d521cca6765f4f7
Vladimir <vgutman95@mail.ru>
2023-02-08 10:15:35 +0600

    'p'

---
+++ assets/js/scripts.js
@@ -0,0 +1,8 @@
+document.addEventListener('DOMContentLoaded', ()=>{
+    const MainSlider = tns({
+        container: '.slider-list',
+        items: 1,
+        controls:false,
+        dots:false
+    });
+});
---
+++ js/scripts.js
@@ -0,0 +1,11 @@
+"use strict";
+
+document.addEventListener('DOMContentLoaded', function () {
+  var MainSlider = tns({
+    container: '.slider-list',
+    items: 1,
+    controls: false,
+    dots: false
+  });
+});
+//# sourceMappingURL=scripts.js.map
